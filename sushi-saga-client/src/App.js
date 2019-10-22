ee5cf5e9a5c21dba403f81d398f54e6c2cf549fa
github username <github email address>
2019-10-22 11:49:13 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,15 +6,50 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushi: [],
+    sushiEaten: [],
+    start: 0,
+    budget: 100
+  }
+
+
+  sushiClickHandler = (event) => {
+    this.setState({start: (this.state.start + 4)})
+
+  }
+
+  buySushi = (sushiObj) => {
+    sushiObj.img_url = null
+    if (this.state.budget > sushiObj.price) {
+    this.setState({
+        sushiEaten: [...this.state.sushiEaten, sushiObj],
+        budget: this.state.budget-sushiObj.price})
+    }
+      else { 
+        alert(`Sorry, you don't have enought money to buy this sushi`)
+      }
+    }
+
+
+  
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer start={this.state.start} allSushi={this.state.sushi} clickHandler={this.sushiClickHandler} buySushi={this.buySushi}/>
+        <Table budget={this.state.budget} sushiEaten={this.state.sushiEaten}/>
       </div>
     );
   }
+
+  componentDidMount(){
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({sushi: data}))
+  }
+
 }
 
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,20 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  
+  //When the sushi is click, the sushi's information should be used to
+  //change the remaining budget
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          (props.allSushi.slice(props.start, (props.start + 4))).map(sushiObj => <Sushi clickHandler={props.buySushi} info={sushiObj} />)
         }
-        <MoreButton />
+        <MoreButton clickHandler={props.clickHandler}/>
       </div>
     </Fragment>
   )
